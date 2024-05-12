6ac59e42042409983e9179364ef014ca23cec786
hajubal <hajubal@gmail.com>
2024-05-12 11:28:07 +0900

    Feat: grammar update select

---
+++ select/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg := <-c1:
+			fmt.Println("Received: ", msg)
+		case msg := <-c2:
+			fmt.Println("Received: ", msg)
+		}
+	}
+}
